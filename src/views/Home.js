a1dd4b0f8a11ba1075995b7f5d90a99706cc37cc
Tomas Gonzalez <tomasg2704@gmail.com>
2021-01-27 21:58:56 +0100

    First commit

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+export const  Home = () => {
+    //const [count, setCount] = useState(0);
+
+    // useEffect(() => {
+    //   document.title = `You clicked ${count} times`;
+    // });
+
+    return(
+        <h1>Home</h1>
+    );
+}
